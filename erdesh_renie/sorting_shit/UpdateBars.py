83caa89bf670fb7def250e767c78c5d0df622246
nameLess_one <nameless_one@domain.org>
2014-06-24 14:44:44 +0300

    good sorting visualization client

--- erdesh_renie/sorting_shit/UpdateBars.py
+++ erdesh_renie/sorting_shit/UpdateBars.py
@@ -4,9 +4,10 @@ from matplotlib.container import BarContainer
 
 class BarUpdate(object):
 
-    def __init__(self, ax, representations):
+    def __init__(self, ax, representations, period=1):
         self.representations = representations
         self.ax = ax
+        self.period = period
 
         first_repro = self.representations[0]
         self.bars = ax.bar(range(0, len(first_repro.visual_y)), first_repro.visual_y,color = 'g')
@@ -23,11 +24,13 @@ class BarUpdate(object):
         not_affected_by_perm_x = [i for i in range(0, len(repre.visual_y)) if i not in repre.affected_indexes]
 
         green = (0.0, 0.5, 0.0, 1.0)
-        light_green = (0.0, 0.5, 0.0, 0.5)
+        yellow = (0.5, 0.0, 0.0, 1.0)
         for x_index in affected_by_perm_x:
-            list_inner[x_index].set_facecolor(light_green)
+            list_inner[x_index].set_facecolor(yellow)
+            list_inner[x_index].set_edgecolor(yellow)
         for x_index in not_affected_by_perm_x:
             list_inner[x_index].set_facecolor(green)
+            list_inner[x_index].set_edgecolor(green)
 
 
     def init(self):
@@ -36,7 +39,7 @@ class BarUpdate(object):
 
     def __call__(self, i):
         self.ax.cla()
-        effective_index = i%len(self.representations)
+        effective_index = (i*self.period)%len(self.representations)
         self.set_state(self.representations[effective_index])
         return self.bars
 
--- erdesh_renie/sorting_shit/sorting_client.py
+++ erdesh_renie/sorting_shit/sorting_client.py
@@ -5,10 +5,11 @@ from Insertion import Insertion
 from Shell_Sort import Shell_Sort
 import random
 from UpdateBars import BarUpdate
+import math
 
 sorting_algorithm = Selection
-n_elements  = 200
-array = [random.uniform(0.0, 1.0) for i in range(n_elements)]
+n_elements  = 1000
+array = [i**2*math.fabs(math.sin(i))*random.uniform(0.0, 1.0) for i in range(n_elements)]
 fig = plt.figure(figsize=(16,9), dpi=200)
 ax = fig.add_subplot(1, 1, 1)
 
@@ -16,9 +17,9 @@ sort =  sorting_algorithm(array)
 sort.sort()
 
 
-graphic = BarUpdate(ax, sort.representations_of_states)
+graphic = BarUpdate(ax, sort.representations_of_states, 15)
 
 anim = FuncAnimation(fig, graphic, len(sort.representations_of_states), init_func=graphic.init,
-        interval=10, blit=True)
+        interval=1, blit=True)
 
 plt.show()
